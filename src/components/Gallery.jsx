a3b14deaf472f9e1a9d3b1ddf96ab1a5d497f4b1
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2024-04-20 14:41:27 -0400

    centered new featured home text

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -14,7 +14,7 @@ export default function Gallery({ galleries = [] }) {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
                   <span className="relative inline-flex rounded-full  h-3 w-3 bg-sky-500"></span>
                </span>
-               <span className="">
+               <span className="text-center">
                   &rarr; Click here for a recent Whole House Remodel &larr;
                </span>
                <span className="relative flex w-3 h-3">
